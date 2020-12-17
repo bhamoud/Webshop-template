d6f3a96be647da0222d9610364e8b87b94240285
chonzin <bassem_hamoud@live.nl>
2020-12-17 21:29:17 +0100

    added components and services
    Signed-off-by: chonzin <bassem_hamoud@live.nl>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/about', pathMatch:'full'},
+  {path: 'about', component: AboutComponent},
+  {path: 'settings', component: SettingsComponent},
+
+  {path: '**', redirectTo: '/about', pathMatch:'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminComponent } from './admin/admin.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SharedComponent } from './shared/shared.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AboutComponent,
+    ServicesComponent,
+    SettingsComponent,
+    SharedComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
