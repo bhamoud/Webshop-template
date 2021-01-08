2b025e71e75189a2effd42e81fbab8767535f523
chonzin <bassem_hamoud@live.nl>
2021-01-08 13:18:00 +0100

    .
    Signed-off-by: chonzin <bassem_hamoud@live.nl>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule, 
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule]
 })
